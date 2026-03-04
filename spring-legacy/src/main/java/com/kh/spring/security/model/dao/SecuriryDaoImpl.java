986a4bc0f8e689bbc2d6bef50136a0055da18395
bujin21 <cvj0810@naver.com>
2026-03-04 17:50:43 +0900

    spring 4일차 security

---
+++ spring-legacy/src/main/java/com/kh/spring/security/model/dao/SecuriryDaoImpl.java
@@ -0,0 +1,20 @@
+package com.kh.spring.security.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class SecuriryDaoImpl implements SecurityDao{
+
+	private final SqlSessionTemplate session;
+	
+	@Override
+	public UserDetails loadUserByUserName(String username) {
+		return session.selectOne("security.loadUserByUsername", username);
+	}
+
+}
